ecef57479e599d2796f0850f1dee489d707f8e09
Braden Bax <Braden.Bax@vu.com>
2022-02-05 17:15:04 -0600

    1

--- payCalculator.py
+++ payCalculator.py
@@ -2,7 +2,14 @@ def calculatePay():
     
     # This first line is provided for you
     hrs = input("Enter Hours:")
-    
+    rate = input("Enter Rate:")
+    othrs = float(hrs) - 40
+    otrate = float(rate) * 1.5
+    newhrs = float(hrs) - float(othrs)
+    pay = float(newhrs)*float(rate)
+    otpay = float(othrs) * float(otrate)
+    totalpay = (pay) + (otpay)
+    print (totalpay)
     # end assignment
 
 ## if you want to test locally before you try to sync
